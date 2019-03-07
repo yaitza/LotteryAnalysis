75718828fc06eaca1e3c78ba12e1b94cf83a33ab
yangzhijie <yangzhijie@aerozhonghuan.com>
2019-03-07 17:38:50 +0800

    红球图片生成

--- DataShow/DataVisual.py
+++ DataShow/DataVisual.py
@@ -16,70 +16,68 @@ from GenerateLotteryCode.SqliteOperator import *
 from DataShow import DataHandle
 
 
-class DataVisual:
-    def __init__(self):
-        self.sql = SqliteOperator()
-        print("init success!")
-
-    def blue_statistics_visual(self):
-        blue_ball, blue_statistics = self.sql.get_blue_statistics()
-        show_ball = []
-        for ball in blue_ball:
-            show_ball.append(int(ball))
-        pyplot.xlabel(r"Blue Balls")
-        pyplot.ylabel(r"Blue Balls Count")
-        pyplot.title(r"Blue Balls Statistics")
-        pyplot.xlim((0, 17))
-        pyplot.ylim((0, 100))
-        pyplot.xticks(range(0, 17, 1))
-        pyplot.yticks(range(0, 100, 10))
-
-        pyplot.bar(show_ball, blue_statistics)
-        for x, y in zip(show_ball, blue_statistics):
-            pyplot.text(x, y+0.5, y, ha='center')
-        pyplot.grid(axis="y")
-        pyplot.savefig(r"{0}\..\image\BlueBallsStatistics.png".format(os.getcwd()), dpi=200)
-        pyplot.show()
-
-    def red_statistics_visual(self):
-        red_balls, red_statistics = DataHandle.red_statistics()
-        pyplot.figure(figsize=(11, 7))
-        pyplot.xlabel(r"red Balls")
-        pyplot.ylabel(r"Red Balls Count")
-        pyplot.title(r"Red Balls Statistics")
-        pyplot.xlim((0, 34))
-        pyplot.ylim((0, 220))
-        pyplot.xticks(range(0, 34, 1))
-        pyplot.yticks(range(0, 220, 10))
-
-        pyplot.bar(red_balls, red_statistics,facecolor='#f72422', edgecolor='white')
-        for x, y in zip(red_balls, red_statistics):
-            pyplot.text(x, y + 0.5, y, ha='center')
-        pyplot.grid(axis="y")
-        pyplot.savefig(r"{0}\..\image\RedBallsStatistics.png".format(os.getcwd()), dpi=300)
-        pyplot.show()
+sql = SqliteOperator()
 
 
-    def blue_visual(self):
-        blue_ball = self.sql.get_single_value('blueballs')
-        show_balls = []
-        for ball in blue_ball:
-            show_balls.append(int(ball))
+def blue_statistics_visual():
+    blue_ball, blue_statistics = sql.get_blue_statistics()
+    show_ball = []
+    for ball in blue_ball:
+        show_ball.append(int(ball))
+    pyplot.xlabel(r"Blue Balls")
+    pyplot.ylabel(r"Blue Balls Count")
+    pyplot.title(r"Blue Balls Statistics, Count:{0}".format(sum(blue_statistics)))
+    pyplot.xlim((0, 17))
+    pyplot.ylim((0, 100))
+    pyplot.xticks(range(0, 17, 1))
+    pyplot.yticks(range(0, 100, 10))
 
-        pyplot.figure('Scatter figure')
-        ax = pyplot.gca()
-        ax.scatter(range(0, 945), show_balls, c='r', s=3)
-        pyplot.ylim((0, 16))
-        pyplot.xlim((0, 1000))
-        pyplot.yticks(range(0, 18, 1))
-        pyplot.xticks(range(-10, 1000, 100))
+    pyplot.bar(show_ball, blue_statistics)
+    for x, y in zip(show_ball, blue_statistics):
+        pyplot.text(x, y+0.5, y, ha='center')
+    pyplot.grid(axis="y")
+    pyplot.savefig(r"{0}\..\image\BlueBallsStatistics.png".format(os.getcwd()), dpi=200)
+    pyplot.show()
 
-        pyplot.grid()
-        pyplot.show()
+
+def red_statistics_visual():
+    red_balls, red_statistics = DataHandle.red_statistics()
+    pyplot.figure(figsize=(11, 7))
+    pyplot.xlabel(r"red Balls")
+    pyplot.ylabel(r"Red Balls Count")
+    pyplot.title(r"Red Balls Statistics, Count:{0}".format(sum(red_statistics)))
+    pyplot.xlim((0, 34))
+    pyplot.ylim((0, 220))
+    pyplot.xticks(range(0, 34, 1))
+    pyplot.yticks(range(0, 220, 10))
+
+    pyplot.bar(red_balls, red_statistics, facecolor='#f72422', edgecolor='white')
+    for x, y in zip(red_balls, red_statistics):
+        pyplot.text(x, y + 0.5, y, ha='center')
+    pyplot.grid(axis="y")
+    pyplot.savefig(r"{0}\..\image\RedBallsStatistics.png".format(os.getcwd()), dpi=300)
+    pyplot.show()
+
+
+def blue_visual():
+    blue_ball = sql.get_single_value('blueballs')
+    show_balls = []
+    for ball in blue_ball:
+        show_balls.append(int(ball))
+
+    pyplot.figure('Scatter figure')
+    ax = pyplot.gca()
+    ax.scatter(range(0, 945), show_balls, c='r', s=3)
+    pyplot.ylim((0, 16))
+    pyplot.xlim((0, 1000))
+    pyplot.yticks(range(0, 18, 1))
+    pyplot.xticks(range(-10, 1000, 100))
+
+    pyplot.grid()
+    pyplot.show()
 
 
 if __name__ == "__main__":
-    dv = DataVisual()
-    # dv.blue_visual()
-    dv.blue_statistics_visual()
-    dv.red_statistics_visual()
+    # blue_visual()
+    blue_statistics_visual()
+    red_statistics_visual()
